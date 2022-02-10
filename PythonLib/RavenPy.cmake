4c8eaf8cc2ea6c8f6271c9d6063dc7a07936d3ec
ivujevic <ivan.vujevic92@gmail.com>
2022-02-10 17:03:59 +0100

    init refactor
    Refactore in more structured way. Graph structure without member functions. It's only first step of refactoring. The next step should be to refactore piles

--- PythonLib/RavenPy.cmake
+++
@@ -1,8 +0,0 @@
-if (NOT TARGET RavenPy)
-
-    include(${CMAKE_CURRENT_LIST_DIR}/RavenPy.srcs.cmake)
-
-    pybind11_add_module(RavenPy ${SOURCES})
-    target_link_libraries(RavenPy PRIVATE RavenLib bioparser::bioparser)
-
-endif()
--- PythonLib/RavenPy.srcs.cmake
+++
@@ -1,8 +0,0 @@
-set(SOURCES "")
-
-set(SOURCES
-        ${CMAKE_CURRENT_LIST_DIR}/Source/RavenPy.cpp
-        )
-
-source_group("Sources" FILES ${SOURCES})
-list(APPEND SOURCES ${Sources})
