42953f2afb6f5f708c0ea5b3cee2351c58df9d28
adolmac <adolmac@gmail.com>
2022-04-30 17:02:38 +0000

    Added biosoup::NucleicAcid and biosoup::Overlap to python bindings.

--- PythonLib/src/ravenpy.cc
+++ PythonLib/src/ravenpy.cc
@@ -22,6 +22,7 @@
 #include "raven/io.h"
 
 namespace py = pybind11;
+using namespace pybind11::literals;
 
 std::atomic<std::uint32_t> biosoup::NucleicAcid::num_objects{0};
 
@@ -69,6 +70,58 @@ struct OverlapsHandle {
 PYBIND11_MODULE(ravenpy, m) {
   m.doc() = "raven";
 
+  py::class_<biosoup::NucleicAcid>(m, "NucleicAcid")
+      .def(py::init<const std::string&, const std::string&>())
+      .def(py::init<const std::string&, const std::string&,
+                    const std::string&>())
+      .def(
+          "__deepcopy__",
+          [](const biosoup::NucleicAcid& seq,
+             py::dict) -> biosoup::NucleicAcid { return seq; },
+          "memo"_a)
+      .def("code", &biosoup::NucleicAcid::Code)
+      .def("score", &biosoup::NucleicAcid::Score)
+      .def("inflate_data", &biosoup::NucleicAcid::InflateData,
+           py::arg("i") = 0U,
+           py::arg("len") = std::numeric_limits<std::uint32_t>::max())
+      .def("inflate_quality", &biosoup::NucleicAcid::InflateQuality,
+           py::arg("i") = 0U,
+           py::arg("len") = std::numeric_limits<std::uint32_t>::max())
+      .def("reverse_and_complement",
+           &biosoup::NucleicAcid::ReverseAndComplement)
+      .def_readwrite("id", &biosoup::NucleicAcid::id)
+      .def_readwrite("name", &biosoup::NucleicAcid::name)
+      .def_readonly("__len__", &biosoup::NucleicAcid::inflated_len);
+
+  py::class_<biosoup::Overlap>(m, "Overlap")
+      .def(py::init<std::uint32_t, std::uint32_t, std::uint32_t, std::uint32_t,
+                    std::uint32_t, std::uint32_t, std::uint32_t, bool>(),
+           py::arg("lhs_id"), py::arg("lhs_begin"), py::arg("lhs_end"),
+           py::arg("rhs_id"), py::arg("rhs_begin"), py::arg("rhs_end"),
+           py::arg("score"), py::arg("strand") = true)
+      .def(py::init<std::uint32_t, std::uint32_t, std::uint32_t, std::uint32_t,
+                    std::uint32_t, std::uint32_t, std::uint32_t,
+                    const std::string&, bool>(),
+           py::arg("lhs_id"), py::arg("lhs_begin"), py::arg("lhs_end"),
+           py::arg("rhs_id"), py::arg("rhs_begin"), py::arg("rhs_end"),
+           py::arg("score"), py::arg("alignment"), py::arg("strand") = true)
+      .def(
+          "__deepcopy__",
+          [](const biosoup::Overlap& ovlp, py::dict) -> biosoup::Overlap {
+            return ovlp;
+          },
+          "memo"_a)
+      .def_readwrite("lhs_id", &biosoup::Overlap::lhs_id)
+      .def_readwrite("lhs_begin", &biosoup::Overlap::lhs_begin)
+      .def_readwrite("lhs_end", &biosoup::Overlap::lhs_end)
+      .def_readwrite("rhs_id", &biosoup::Overlap::rhs_id)
+      .def_readwrite("rhs_begin", &biosoup::Overlap::rhs_begin)
+      .def_readwrite("rhs_end", &biosoup::Overlap::rhs_end)
+      .def_readwrite("score", &biosoup::Overlap::score)
+      .def_readwrite("strand", &biosoup::Overlap::strand)
+      .def_readwrite("alignment", &biosoup::Overlap::alignment);
+
+
   py::class_<raven::AlignCfg>(m, "AlignCfg")
       .def(py::init<std::int8_t, std::int8_t, std::int8_t>())
       .def_readwrite("match", &raven::AlignCfg::match)
@@ -99,18 +152,24 @@ PYBIND11_MODULE(ravenpy, m) {
       m, "ThreadPool")
       .def(py::init<std::uint32_t>());
 
+  py::class_<raven::Graph>(m, "Graph").def(py::init<>());
+
   py::class_<SequencesHandle, std::shared_ptr<SequencesHandle>>(
       m, "SequencesHandle")
       .def(py::init<const std::vector<std::string>&>());
 
   py::class_<OverlapsHandle, std::shared_ptr<OverlapsHandle>>(
       m, "OverlapsHandle")
-      .def(py::init<std::shared_ptr<SequencesHandle>>());
+      .def(py::init<std::shared_ptr<SequencesHandle>>())
+      .def_readonly("overlaps", &OverlapsHandle::overlaps);
 
   m.def("reset_seq_id_cnt",
         []() -> void { biosoup::NucleicAcid::num_objects.store(0); });
 
-  py::class_<raven::Graph>(m, "Graph").def(py::init<>());
+  m.def("load_sequences",
+        [](std::vector<std::string>& paths) -> std::vector<std::unique_ptr<biosoup::NucleicAcid>> {
+          return LoadSequences(paths);
+        });
 
   m.def("construct_graph",
         [](raven::Graph& graph, std::shared_ptr<SequencesHandle> seqs_handle,
@@ -148,8 +207,6 @@ PYBIND11_MODULE(ravenpy, m) {
 
   m.def("graph_load_gfa", raven::LoadGfa);
 
-  py::class_<biosoup::Overlap>(m, "Overlap").def(py::init<>());
-  py::class_<biosoup::NucleicAcid>(m, "NucleicAcid").def(py::init<>());
   py::class_<ram::MinimizerEngine>(m, "MinimizerEngine")
       .def(py::init<std::shared_ptr<thread_pool::ThreadPool>, std::uint32_t, std::uint32_t>());
  
